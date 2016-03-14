75558c525d28fa03b0470809b8fe09c6050d7b72
Gordon Yoon <gordonyoon@gmail.com>
2016-03-14 14:33:37 -0700

    Cleanup NowPlayingReceiver

--- app/src/main/java/com/example/gordonyoon/perfectplaylist/spotify/NowPlayingReceiver.kt
+++ app/src/main/java/com/example/gordonyoon/perfectplaylist/spotify/NowPlayingReceiver.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import com.example.gordonyoon.perfectplaylist.androidextentions.getAppContext
 import com.example.gordonyoon.perfectplaylist.rx.RxBus
-import timber.log.Timber
 import javax.inject.Inject
 
 class NowPlayingReceiver : BroadcastReceiver() {
@@ -14,38 +13,60 @@ class NowPlayingReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         context.getAppContext().spotifyComponent.inject(this)
-        // This is sent with all broadcasts, regardless of type. The value is taken from
-        // System.currentTimeMillis(), which you can compare to in order to determine how
-        // old the event is.
-        val timeSentInMs = intent.getLongExtra("timeSent", 0L)
 
-        val action = intent.action
-        if (action.equals(BroadcastTypes.METADATA_CHANGED)) {
-            val trackId = intent.getStringExtra("id")
-            val artistName = intent.getStringExtra("artist")
-            val albumName = intent.getStringExtra("album")
-            val trackName = intent.getStringExtra("track")
-            val trackLengthInSec = intent.getIntExtra("length", 0)
-            bus.send(NowPlayingTrack(trackId, artistName, albumName, trackName, trackLengthInSec))
-        } else if (action.equals(BroadcastTypes.PLAYBACK_STATE_CHANGED)) {
-            val playing = intent.getBooleanExtra("playing", false)
-            val positionInMs = intent.getIntExtra("playbackPosition", 0)
-            bus.send(NowPlayingStateChange(playing, positionInMs))
-        } else if (action.equals(BroadcastTypes.QUEUE_CHANGED)) {
-            bus.send(NowPlayingQueueChange())
+        when (intent.action) {
+            BroadcastTypes.METADATA_CHANGED       -> bus.send(NowPlayingTrack(intent))
+            BroadcastTypes.PLAYBACK_STATE_CHANGED -> bus.send(NowPlayingStateChange(intent))
+            BroadcastTypes.QUEUE_CHANGED          -> bus.send(NowPlayingQueueChange(intent))
         }
     }
 
     object BroadcastTypes {
-        const val SPOTIFY_PACKAGE = "com.spotify.music"
+        const val SPOTIFY_PACKAGE        = "com.spotify.music"
         const val PLAYBACK_STATE_CHANGED = SPOTIFY_PACKAGE + ".playbackstatechanged"
-        const val QUEUE_CHANGED = SPOTIFY_PACKAGE + ".queuechanged"
-        const val METADATA_CHANGED = SPOTIFY_PACKAGE + ".metadatachanged"
+        const val QUEUE_CHANGED          = SPOTIFY_PACKAGE + ".queuechanged"
+        const val METADATA_CHANGED       = SPOTIFY_PACKAGE + ".metadatachanged"
     }
 
-    data class NowPlayingTrack(val id: String, val artist: String, val album: String, val track: String, val length: Int)
+    data class NowPlayingTrack(val id: String, val artist: String, val album: String, val track: String, val length: Int) {
 
-    data class NowPlayingStateChange(val playing: Boolean, val playbackPosition: Int)
+        constructor(intent: Intent): this(
+                intent.getStringExtra("id"),
+                intent.getStringExtra("artist"),
+                intent.getStringExtra("album"),
+                intent.getStringExtra("track"),
+                intent.getIntExtra("length", 0)
+        ) {
+            if (intent.action != BroadcastTypes.METADATA_CHANGED
+                    || !intent.hasExtra("id")
+                    || !intent.hasExtra("artist")
+                    || !intent.hasExtra("album")
+                    || !intent.hasExtra("track")
+                    || !intent.hasExtra("length")) {
+                throw IllegalArgumentException()
+            }
+        }
+    }
 
-    class NowPlayingQueueChange()
+    data class NowPlayingStateChange(val playing: Boolean, val playbackPosition: Int) {
+
+        constructor(intent: Intent) : this(
+                intent.getBooleanExtra("playing", false),
+                intent.getIntExtra("playbackPosition", 0)
+        ) {
+            if (intent.action != BroadcastTypes.PLAYBACK_STATE_CHANGED
+                    || !intent.hasExtra("playing")
+                    || !intent.hasExtra("playbackPosition")) {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
+    class NowPlayingQueueChange(intent: Intent) {
+        init {
+            if (intent.action != BroadcastTypes.PLAYBACK_STATE_CHANGED) {
+                throw IllegalArgumentException()
+            }
+        }
+    }
 }
