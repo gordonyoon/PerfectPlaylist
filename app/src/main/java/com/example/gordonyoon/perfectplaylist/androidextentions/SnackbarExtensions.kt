075e251bad8277a8cd52d57a8218bd98fdc1dba1
Gordon Yoon <gordonyoon@gmail.com>
2016-03-16 23:30:32 -0700

    Add convenience methods to Snackbar

---
+++ app/src/main/java/com/example/gordonyoon/perfectplaylist/androidextentions/SnackbarExtensions.kt
@@ -0,0 +1,13 @@
+package com.example.gordonyoon.perfectplaylist.androidextentions
+
+import android.app.Activity
+import android.support.annotation.StringRes
+import android.support.design.widget.Snackbar
+
+fun Snackbar.make(activity: Activity, text: CharSequence, @Snackbar.Duration duration: Int): Snackbar {
+    return Snackbar.make(activity.window.decorView.rootView, text, duration)
+}
+
+fun Snackbar.make(activity: Activity, @StringRes resId: Int, @Snackbar.Duration duration: Int): Snackbar {
+    return Snackbar.make(activity.window.decorView.rootView, resId, duration)
+}
