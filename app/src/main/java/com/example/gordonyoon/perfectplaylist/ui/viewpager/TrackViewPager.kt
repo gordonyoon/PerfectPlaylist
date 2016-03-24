bf87e0f043928e9dffc000b7abd9e3dc9874e630
Gordon Yoon <gordonyoon@gmail.com>
2016-03-23 21:25:09 -0700

    Wrap content for the ViewPager

--- app/src/main/java/com/example/gordonyoon/perfectplaylist/ui/viewpager/TrackViewPager.kt
+++ app/src/main/java/com/example/gordonyoon/perfectplaylist/ui/viewpager/TrackViewPager.kt
@@ -10,19 +10,19 @@ import com.antonyt.infiniteviewpager.InfiniteViewPager
  */
 class TrackViewPager(context: Context, attributeSet: AttributeSet): InfiniteViewPager(context, attributeSet) {
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//        var height = 0
-//        for (i in 0..(childCount - 1)) {
-//            val child = getChildAt(i)
-//            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
-//            val h = child.measuredHeight
-//            if (h > height) height = h
-//        }
-//
-//        val adjustedHeightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
-//
-//        super.onMeasure(widthMeasureSpec, adjustedHeightMeasureSpec)
-//    }
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var height = 0
+        for (i in 0..(childCount - 1)) {
+            val child = getChildAt(i)
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
+            val h = child.measuredHeight
+            if (h > height) height = h
+        }
+
+        val adjustedHeightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, adjustedHeightMeasureSpec)
+    }
 
     override fun setAdapter(adapter: PagerAdapter?) {
         if (adapter !is InfinitePagerAdapterWrapper) {
